5b054b4118852d9aaf6905890f447b5d62f968f4
Eric Kim <ericykim5399@gmail.com>
2020-11-11 17:41:33 -0500

    clear repo

--- tipsy-server/src/main/java/com/example/tipsyserver/repositories/DrinkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tipsyserver.repositories;
-
-import com.example.tipsyserver.models.Drink;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DrinkRepository extends CrudRepository<Drink, Integer>{
-}
